b81fc5dbe768c3adea8d26ac61765498dd67d98b
EidesJohnSapugay-UCM <eidesjohn@gmail.com>
2025-09-03 19:59:17 +0800

    Create Comments.java

---
+++ Chapter_1 /Comments.java
@@ -0,0 +1,18 @@
+public class Comments {
+    public static void main(String[] args) {
+        
+        // This is a single-line comment
+        System.out.println("Single-line comment example");
+
+        /* 
+           This is a multi-line comment
+        */
+        System.out.println("Multi-line comment example");
+
+        /**
+         * This is a documentation comment
+         * Used by Javadoc to generate documentation
+         */
+        System.out.println("Documentation comment example");
+    }
+}
