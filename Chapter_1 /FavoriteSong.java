5170c933e8a82059ca296192700b9195f76be17f
EidesJohnSapugay-UCM <eidesjohn@gmail.com>
2025-09-03 19:26:45 +0800

    Create FavoriteSong.java

---
+++ Chapter_1 /FavoriteSong.java
@@ -0,0 +1,8 @@
+public class FavoriteSong {
+    public static void main(String[] args) {
+        System.out.println("Ilusyon);
+        System.out.println("Diwata");
+        System.out.println("Believer ");
+        System.out.println("Binks No Sake");
+    }
+}
