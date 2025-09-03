d7a9158a8a5521d77511213ded738c65dc17a60a
EidesJohnSapugay-UCM <eidesjohn@gmail.com>
2025-09-03 08:36:57 +0800

    Create TableAndChairs.java

---
+++ Chapter_1 /TableAndChairs.java
@@ -0,0 +1,15 @@
+public class TableAndChairs {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 3; i++) {
+            System.out.println("|                               |");
+        }
+
+
+        System.out.println("|_ _ _ _     _ _ _ _     _ _ _ _|");
+
+        for (int i = 1; i <= 3; i++) {
+            System.out.println("|      |     |     |     |      |");
+        }
+    }
+}
