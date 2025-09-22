7dbba0b3809d396652e6c2df5c28124cdc1837a6
EidesJohnSapugay-UCM <eidesjohn@gmail.com>
2025-09-22 09:04:13 +0800

    Update UsingJOptionPaneDialogs.java

--- Chapter_2/UsingJOptionPaneDialogs.java
+++ Chapter_2/UsingJOptionPaneDialogs.java
@@ -7,24 +7,22 @@ public class UsingJOptionPaneDialogs {
             
             String name = JOptionPane.showInputDialog(null, "What is your name:");
 
-            
-            if (name == null && !name.isEmpty()) {
-                JOptionPane.showMessageDialog(null, "You must enter your name.");
-                continue; 
+        
+            if (name == null) {
+                System.exit(0);
             }
 
-            
+           
             int response = JOptionPane.showConfirmDialog(null,
-                    "Do you want to display your name? " ,
+                    "Do you want to display your name?",
                     "Confirm Name",
                     JOptionPane.YES_NO_OPTION);
 
             
             if (response == JOptionPane.YES_OPTION) {
                 JOptionPane.showMessageDialog(null, "Hi " + name + "!");
-                break; 
+                break;
             }
-           
         }
     }
 }
